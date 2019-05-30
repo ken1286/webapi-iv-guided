cb6cd6ed67c66138b9e7a2f06035a0a077a66ccb
ken1286 <kindle1286@gmail.com>
2019-05-29 20:32:37 -0700

    update

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const server = require('./api/server.js');
 
 const port = process.env.PORT || 4000;
 server.listen(port, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+  console.log(`\n*** Server Running on ${port} ***\n`);
 });
